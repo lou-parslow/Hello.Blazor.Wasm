3c1975d4d7e0542e838eacc873fd24fa1dbba8f2
Lou Parslow <lou.parslow@gmail.com>
2023-11-08 07:35:32 -0700

    "integrate"

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,8 @@
 require "raykit"
 
 # https://github.com/lou-parslow/Hello.Blazor.Wasm.git
+# https://<username>.github.io/<repository-name>/
+# https://lou-parslow.github.io/Hello.Blazor.Wasm/
 
 task :setup do
   run "dotnet new blazorwasm -o src/Hello.Blazor.Wasm -n Hello.Blazor.Wasm" unless Dir.exist? "src/Hello.Blazor.Wasm"
@@ -11,6 +13,7 @@ end
 task :build do
   run "dotnet publish src/Hello.Blazor.Wasm/Hello.Blazor.Wasm.csproj -c Release -o dist"
   mkdir "docs" unless Dir.exist? "docs"
-  FileUtils.cp_r "dist/wwwroot/.", "docs"
+  #FileUtils.cp_r "dist/wwwroot/.", "docs"
+  FileUtils.cp_r "dist/.", "docs"
   #run "cp -r dist/wwwroot/* docs"
 end
