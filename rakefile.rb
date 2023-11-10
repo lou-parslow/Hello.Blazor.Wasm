f004b76c8f4460bf2152659b959bfc040a0876e2
Lou Parslow <lou.parslow@gmail.com>
2023-11-10 08:13:01 -0700

    integrate

--- rakefile.rb
+++ rakefile.rb
@@ -21,9 +21,6 @@ end
 task :update_docs => [:build] do
   mkdir "docs" unless Dir.exist? "docs"
   FileUtils.cp_r "#{SITE_DIR}/.", "docs"
-  # for github pages, a .nojekyll file is required to prevent github from ignoring files starting with an underscore
-  #File.open("docs/.nojekyll", "w") { }
-  #Raykit::Text::replace_in_file("docs/index.html", '<base href="/" />', '<base href="/Hello.Blazor.Wasm/" />')
 end
 
 task :default => [:build, :integrate, :push] do
