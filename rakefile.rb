5da244c251816f55b987486485a64862c78f2cfe
Lou Parslow <lou.parslow@gmail.com>
2023-11-10 07:59:07 -0700

    integrate

--- rakefile.rb
+++ rakefile.rb
@@ -3,11 +3,6 @@ require "raykit"
 
 task :build do
   run "dotnet publish src/Hello.Blazor.Wasm/Hello.Blazor.Wasm.csproj -c Release -o dist"
-  #mkdir "docs" unless Dir.exist? "docs"
-  #FileUtils.cp_r "dist/wwwroot/.", "docs"
-  # for github pages, a .nojekyll file is required to prevent github from ignoring files starting with an underscore
-  #File.open("docs/.nojekyll", "w") {}
-  #Raykit::Text::replace_in_file("docs/index.html", '<base href="/" />', '<base href="/Hello.Blazor.Wasm/" />')
 end
 
 task :update_docs => [:build] do
