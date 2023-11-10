fc94e653c051631a3d1d0c98aff5c69e5fa950d7
Lou Parslow <lou.parslow@gmail.com>
2023-11-10 08:09:52 -0700

    integrate

--- rakefile.rb
+++ rakefile.rb
@@ -1,18 +1,19 @@
 # this project may be viewed here: https://lou-parslow.github.io/Hello.Blazor.Wasm/
 VERSION="0.0.0"
-PUBLISH_DIR="artifacts/Hello.Blazor.Wasm.#{VERSION}"
+
 require "raykit"
 
 task :build do
   run "dotnet publish src/Hello.Blazor.Wasm/Hello.Blazor.Wasm.csproj -c Release -o dist"
 
   FileUtils.mkdir("artifacts") unless Dir.exist? "artifacts"
-  FileUtils.mkdir(PUBLISH_DIR) unless Dir.exist? PUBLISH_DIR
-  puts "  copying files to #{PUBLISH_DIR}"
-  FileUtils.cp_r "dist/wwwroot/.", PUBLISH_DIR
+  SITE_DIR="artifacts/Hello.Blazor.Wasm.#{VERSION}"
+  FileUtils.mkdir(SITE_DIR) unless Dir.exist? SITE_DIR
+  puts "  copying files to #{SITE_DIR}"
+  FileUtils.cp_r "dist/wwwroot/.", SITE_DIR
   # for github pages, a .nojekyll file is required to prevent github from ignoring files starting with an underscore
-  File.open("#{PUBLISH_DIR}/.nojekyll", "w") { }
-  Raykit::Text::replace_in_file("#{PUBLISH_DIR}/index.html", '<base href="/" />', '<base href="/Hello.Blazor.Wasm/" />')
+  File.open("#{SITE_DIR}/.nojekyll", "w") { }
+  Raykit::Text::replace_in_file("#{SITE_DIR}/index.html", '<base href="/" />', '<base href="/Hello.Blazor.Wasm/" />')
 end
 
 task :update_docs => [:build] do
