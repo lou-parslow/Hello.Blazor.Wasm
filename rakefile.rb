6a6a4128a1f7173017feaf2b4afbeada3baf21eb
Lou Parslow <lou.parslow@gmail.com>
2023-11-10 08:07:40 -0700

    integrate

--- rakefile.rb
+++ rakefile.rb
@@ -6,6 +6,7 @@ require "raykit"
 task :build do
   run "dotnet publish src/Hello.Blazor.Wasm/Hello.Blazor.Wasm.csproj -c Release -o dist"
 
+  FileUtils.mkdir("artifacts") unless Dir.exist? "artifacts"
   FileUtils.mkdir_p(PUBLISH_DIR) unless Dir.exist? PUBLISH_DIR
   FileUtils.cp_r "dist/wwwroot/.", PUBLISH_DIR
   # for github pages, a .nojekyll file is required to prevent github from ignoring files starting with an underscore
