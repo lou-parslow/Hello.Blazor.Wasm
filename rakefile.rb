8585fd87051b184b6eb19254d4768b1b494fd54b
lou parslow <lou.parslow@gmail.com>
2024-02-14 09:39:19 -0700

    "integrate"

--- rakefile.rb
+++ rakefile.rb
@@ -1,10 +1,19 @@
 # this project may be viewed here: https://lou-parslow.github.io/Hello.Blazor.Wasm/
-VERSION="0.0.0"
+VERSION = "0.0.0"
 require "raykit"
-SITE_DIR="artifacts/Hello.Blazor.Wasm.#{VERSION}"
+SITE_DIR = "artifacts/Hello.Blazor.Wasm.#{VERSION}"
+
+# https://lou-parslow.github.io/Hello.Blazor.Wasm/
+
+task :setup do
+  run "dotnet add src/Hello.Blazor.Wasm/Hello.Blazor.Wasm.csproj package Microsoft.Extensions.DependencyInjection"
+end
 
 task :build do
- FileUtils.cp("README.md","src/Hello.Blazor.Wasm/wwwroot/")
+  start_task :build
+  try "rufo ."
+  run "dotnet build src/Hello.Blazor.Wasm/Hello.Blazor.Wasm.csproj"
+  FileUtils.cp("README.md", "src/Hello.Blazor.Wasm/wwwroot/")
   run "dotnet publish src/Hello.Blazor.Wasm/Hello.Blazor.Wasm.csproj -c Release -o dist"
   FileUtils.mkdir("artifacts") unless Dir.exist? "artifacts"
   FileUtils.mkdir(SITE_DIR) unless Dir.exist? SITE_DIR
@@ -20,5 +29,5 @@ task :update_docs => [:build] do
   FileUtils.cp_r "#{SITE_DIR}/.", "docs"
 end
 
-task :default => [:build,:update_docs, :integrate, :push] do
+task :default => [:build, :update_docs, :integrate, :push] do
 end
--- src/Hello.Blazor.Wasm/Program.cs
+++ src/Hello.Blazor.Wasm/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Hello.Blazor.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,20 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Configure authentication
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure authentication provider (Azure AD/B2C)
+                string tenantId = "0520d39c-3412-4086-b59b-bcd6aed60f47";
+                string clientId = "31676f1f-f392-402b-b01c-9bd220d8a67f";
+                options.ProviderOptions.Authority = $"https://login.microsoftonline.com/{tenantId-id}/v2.0/";
+                options.ProviderOptions.ClientId = $"{clientId-id}";
+
+                // Add scopes if needed
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("profile");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("email");
+            });
 
 await builder.Build().RunAsync();
